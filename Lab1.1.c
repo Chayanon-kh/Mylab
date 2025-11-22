2fa4dcfb5b15802876529483f2a058ffd26dc1ba
Chayanon_kh <chayanon5826@gmail.com>
2025-11-22 16:16:41 +0700

    Add files via upload

--- Lab1.1.c
+++ Lab1.1.c
@@ -5,15 +5,15 @@ int main() {
     if (scanf("%d", &N) != 1) {
         printf("Error.\n");
         return 1; 
-    }
+    }//Endif
 
     if (N < 0) {           
         printf("Error\n");
-    }
+    }//Endif
 
     for (i = 0; i < N; i++) {
         printf("Hello Loop!\n"); 
-    }
+    }//Endloop.
     
     return 0;
 }
