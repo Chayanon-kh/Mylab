dbcd54f7e1b32bd99f70b3ff27dc8a7b6ef90dee
Chayanon_kh <chayanon5826@gmail.com>
2025-12-28 17:07:54 +0700

    Add files via upload

---
+++ 6.5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
+
+int count_vowels_with_pointer(char *str) {
+    int count = 0;
+    char *char_ptr = str;
+
+    while (*char_ptr != '\0') { 
+        switch (*char_ptr) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                count++;
+                break;
+        }
+        char_ptr++; 
+    }
+    return count;
+}
+
+int main() {
+    char sentence[MAX_SIZE];
+    int vowel_count;
+
+    printf("Enter a sentence: ");
+    if (fgets(sentence, MAX_SIZE, stdin) == NULL) {
+        return 1;
+    }
+
+    sentence[strcspn(sentence, "\n")] = '\0';
+
+    vowel_count = count_vowels_with_pointer(sentence);
+
+    printf("\n--- STRING ANALYSIS REPORT ---\n");
+    printf("Input String: \"%s\"\n", sentence);
+    printf("Total Vowel Count: %d\n", vowel_count);
+
+    return 0;
+}
